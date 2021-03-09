1e638295395d90c27985cfc1f2d885f1688fd118
fengkaiwater <328458211@qq.com>
2021-03-09 13:18:41 +0800

    add bootstrap flash function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Go Back！ ---up"
+    # flash[:alert] = "Illegal Intrude! ---middle"
+    # flash[:warning] = "Attack! ---down"
+  end
 end
